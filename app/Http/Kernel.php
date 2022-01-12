d41df24158042bbbf058934b5a6ba144ff351cc7
Ryo <ryo@ryo.local>
2022-01-12 12:42:28 +0900

    first commit

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -45,6 +45,7 @@ class Kernel extends HttpKernel
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
